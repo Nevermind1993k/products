5add6613da3b7631514cd7dde8b57494b5e020f7
Roman <nevermind1993k@gmail.com>
2018-03-11 20:28:24 +0200

    Initial commit

---
+++ src/main/java/com/nevermind/bu/ProductsApplication.java
@@ -0,0 +1,16 @@
+package com.nevermind.bu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class
+ *
+ * @author Roman Kovaliov
+ */
+@SpringBootApplication
+public class ProductsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsApplication.class, args);
+    }
+}
